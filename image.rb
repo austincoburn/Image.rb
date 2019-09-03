dd50095643602cf7c522a767c935d450f4b531fc
Austin Coburn <austincoburn@austins-mbp.lan>
2019-09-03 13:45:57 -0500

    Image.rb file

---
+++ image.rb
@@ -0,0 +1,22 @@
+class Image
+  attr_accessor :pixel
+
+  def initialize(pixel)
+    @pixel = pixel
+  end
+
+  def output_image
+  	@pixel.each do |line|
+  	  puts line.join
+  	end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
