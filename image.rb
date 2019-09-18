81f023e7cb761b8c147ab9481d917813fae64320
Austin Coburn <52141572+austincoburn@users.noreply.github.com>
2019-09-18 16:30:00 -0500

    Add files via upload

--- image.rb
+++ image.rb
@@ -5,10 +5,41 @@ class Image
     @pixel = pixel
   end
 
-  def output_image
-  	@pixel.each do |line|
-  	  puts line.join
-  	end
+  def image_manipulation
+    ones = []
+    @pixel.each_with_index do |row, row_number|
+    row.each_with_index do |col, col_number|
+      if col == 1
+        ones << [row_number, col_number]
+    end
+  end
+end
+ones
+end
+
+  def blur
+    ones = image_manipulation
+      @pixel.each_with_index do |row, row_number|
+        row.each_with_index do |col, col_number|
+          ones.each do |the_row_number, the_col_number|
+
+          if row_number == the_row_number && col_number == the_col_number
+            @pixel[row_number - 1][col_number] = 1 unless row_number == 0
+            @pixel[row_number + 1][col_number] = 1 unless row_number >= 3
+            @pixel[row_number][col_number - 1] = 1 unless col_number == 0
+            @pixel[row_number][col_number + 1] = 1 unless col_number >= 3
+          end
+        end
+      end
+    end
+  end
+
+
+
+ def output_image
+    @pixel.each do |line|
+      puts line.join
+    end
   end
 end
 
@@ -20,3 +51,9 @@ image = Image.new([
 ])
 
 image.output_image
+puts 
+image.blur
+
+image.output_image
+
+
